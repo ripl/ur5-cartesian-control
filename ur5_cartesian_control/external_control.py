f9fa57ddcfda4998222315830f1553db2cd1be62
Tianchong Jiang <tianchongjiang@gmail.com>
2023-03-15 17:02:06 -0500

    external control, need to debug

---
+++ ur5_cartesian_control/external_control.py
@@ -0,0 +1,111 @@
+#! /usr/bin/env python3
+
+from tkinter import W
+import rospy
+import signal
+from ros_utils import *
+from sensor_msgs.msg import Float32MultiArray
+from robotiq_s_interface import Gripper
+from threading import BoundedSemaphore
+import numpy as np
+
+from scipy.spatial.transform import Rotation as R
+
+from ur5_cartesian_control.trajectory_client import TrajectoryClient, Pose
+
+eps = 1e-3
+
+class JoystickControl:
+    def __init__(self) -> None:
+        # get parameters
+        rospy.init_node('external_cartesian_control')
+
+
+        # NOTE: No idea why ~joy_topic is not found.
+        self.input_topic = rospy.get_param("~joy_topic", '/external_control')
+
+        # Choices: ['basic', 'wide', 'pinch', 'scissor']
+        self.gripper_mode = rospy.get_param("~gripper_mode", "pinch")
+        self.gripper_open_value = rospy.get_param("~gripper_open_value", 0.0)
+        self.gripper_close_value = rospy.get_param("~gripper_close_value", 1.0)
+
+        self.pos_step = rospy.get_param("~pos_step", 0.08)
+        self.rot_step = rospy.get_param("~rot_step", 0.6)
+
+        # subscribe to joy commands
+        self._sub_joy = rospy.Subscriber(self.input_topic, Float32MultiArray, self.control_callback, queue_size=1)
+
+        self.semaphore = BoundedSemaphore()
+
+        # Instantiate TrajectoryClient
+        self.traj_client = TrajectoryClient(confirm_before_motion=False)
+
+        # Instantiate Gripper Interface
+        self.gripper = Gripper(
+            open_value=self.gripper_open_value,
+            close_value=self.gripper_close_value,
+        )
+        self._initialize_gripper()
+
+        # Initial pose
+        self.init_pose = Pose(pos=(0.1, -0.4, 0.4), quat=(0.928, -0.371, 0, 0))
+        self.prev_pose = self.init_pose
+
+        self.preg_grip_input = 1.0  # Close
+
+        # Move to the initial pose
+        self.traj_client.send_cartesian_trajectory([self.prev_pose])
+
+        # NOTE: Is it necessary??
+        # listen for SIGINT
+        signal.signal(signal.SIGINT, self._shutdown)
+
+    def _initialize_gripper(self):
+        self.gripper.activate()
+
+        if self.gripper_mode == 'basic':
+            self.gripper.basic_mode()
+        elif self.gripper_mode == 'wide':
+            self.gripper.wide_mode()
+        elif self.gripper_mode == 'pinch':
+            self.gripper.pinch_mode()
+        elif self.gripper_mode == 'scissor':
+            self.gripper.scissor_mode()
+        else:
+            raise ValueError(f'unknown mode: {self.gripper_mode}')
+
+    def joy_callback(self, msg):
+        # return if another message is using the gripper
+        gripper_is_busy = self.semaphore.acquire(blocking=False)
+        if gripper_is_busy:
+            return
+
+        quat = msg.data[3:7].as_quat()
+
+        traj = [Pose(pos=msg.data[:3], quat=quat)]
+
+        # assert self.pos_step < 0.2, 'step >= 0.1 can be dangerous...'
+
+        self.traj_client.send_cartesian_trajectory(traj, init_time=0.0, time_step=10)
+        ROS_INFO('sending trajectory to the client... DONE')
+
+        # Open / Close the gripper
+        if abs(msg.data[7]) > eps:
+            if msg.data[7] > eps:
+                self.gripper.close()
+            else:
+                self.gripper.open()
+
+        # release lock and exit
+        self.semaphore.release()
+
+    def _shutdown(self, *args):
+        ROS_INFO('Shutting down...')
+        self.gripper.shutdown()
+
+
+if __name__ == '__main__':
+    joy_con = JoystickControl()
+
+    # keep spinning
+    rospy.spin()
